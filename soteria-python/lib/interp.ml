6b92bb5c754f31455b4398a782bcd397e32375c0
Timothée MENEUX <timotheemeneux@Timothees-MacBook-Air.local>
2026-06-22 17:37:38 +0100

    beginnig of interp.ml

--- soteria-python/lib/interp.ml
+++ soteria-python/lib/interp.ml
@@ -1,14 +1,11 @@
 open Pytecode
 open Pytecode.Phir
 module Symex = Soteria.Symex.Make (Values.Solver.Z3_solver)
-
-(* open Symex *)
+open Symex
 open Symex.Syntax
 open Values
 open Typed.Infix
 open Typed.Syntax
-
-
 module String_map = Map.Make (Soteria.Soteria_std.String)
 
 type subst = Svalue.t String_map.t
@@ -21,13 +18,14 @@ let eval_var (v : var) : Svalue.t =
   | Name n -> Svalue.(mk_var (Var.of_string n) TNull)
   | Global g -> Svalue.(mk_var (Var.of_string g) TNull)
 
-let rec eval_const (subst : subst) (c : code) (co : Ast.const) : Svalue.t =
+let rec eval_const (subst : subst) (c : code) (co : Ast.const) :
+    (Svalue.t, 'err, 'a) Symex.Result.t =
   match co with
-  | None_ -> Svalue.(None_ <| TNone_)
-  | Bool b -> Svalue.of_bool b
-  | Int i -> Svalue.SInt.int_z i
-  | Float f -> Svalue.SFloat.float f
-  | Complex { re:_ ; im:_ } -> failwith "ToDo"
+  | None_ -> Svalue.(None_ <| TNone_) |> Result.ok
+  | Bool b -> Svalue.of_bool b |> Result.ok
+  | Int i -> Svalue.SInt.int_z i |> Result.ok
+  | Float f -> Svalue.SFloat.float f |> Result.ok
+  | Complex { re : _; im : _ } -> failwith "ToDo"
   | Str _ -> failwith "ToDo"
   | Bytes _ -> failwith "ToDo"
   | Tuple _ -> failwith "ToDo"
@@ -36,17 +34,17 @@ let rec eval_const (subst : subst) (c : code) (co : Ast.const) : Svalue.t =
   | Ellipsis -> failwith "ToDo"
 
 let rec eval_value (subst : subst) (c : code) (v : value) :
-    Svalue.t =
+    (Svalue.t, 'err, 'a) Symex.Result.t =
   match v with
   | Stack -> failwith "Todo"
-  | Null -> Svalue.null
+  | Null -> Svalue.null |> Result.ok
   | Const co -> eval_const subst c co
   | Code c -> failwith "ToDo"
   | Var v -> failwith "ToDo"
 
 and eval_instr (subst : subst) (c : code) (i : instr) :
-    (Svalue.t, error) Result.t Symex.t =
+    (Svalue.t, 'err, 'a) Symex.Result.t =
   match i with Assign (x, v) -> failwith "ToDo" | _ -> failwith "ToDo"
 
-and eval_code (subst : subst) (c : code) : (Svalue.t, error) Result.t Symex.t
-   = match c with | _ -> failwith "ToDo"
+and eval_code (subst : subst) (c : code) : (Svalue.t, 'err, 'a) Symex.Result.t =
+  match c with _ -> failwith "ToDo"
